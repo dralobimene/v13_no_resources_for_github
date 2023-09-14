f15a21e8381eeb995d958fe0abd0b1e4a5fe5657
sambano <sambano@debian>
2023-09-14 13:17:46 +0200

    upload shell script to install project dependencies

---
+++ pip_install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+python -m pip install --upgrade pip
+pip install pygame
+pip install pygame_gui
+pip install coverage
+pip install "python-lsp-server[all]"
+pip install mock
+pip install opencv-python-headless
+pip install pillow
